2cf322b6c94aba7a79f0b44d407745d523e02d2d
Dernerd <dern3rd@outlook.at>
2021-09-19 11:26:04 +0200

    +Updater

--- functions.php
+++ functions.php
@@ -5,3 +5,11 @@ add_action( 'wp_enqueue_scripts', 'upfront_child_enqueue_styles' );
 function upfront_child_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+
+require 'psource/theme-updates/theme-update-checker.php';
+$MyThemeUpdateChecker = new ThemeUpdateChecker(
+	'upfront-child', 
+	'https://n3rds.work//wp-update-server/?action=get_metadata&slug=upfront-child' 
+);
+
+
